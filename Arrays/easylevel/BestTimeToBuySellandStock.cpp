1003ae4401703067ef0662e8469ca940b11d68ff
jasraj gupta <jasrajgupta1@gmail.com>
2025-10-07 21:17:29 +0530

    max profit

---
+++ Arrays/easylevel/BestTimeToBuySellandStock.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+#include<climits>
+#include<algorithm>
+
+int maxProfit(vector<int>& prices) {
+    int maxProfit = 0 , bestBuy = prices[0];
+    for(int i = 1; i<prices.size(); i++){
+        if(prices[i]>bestBuy) {
+          maxProfit = max(maxProfit, prices[i] - bestBuy);
+        }
+      
+            bestBuy =  min(bestBuy, prices[i]);
+        }
+    
+    return maxProfit;
+
+};
+
+
+int main() {
+  vector<int>prices = {7,1, 5, 3, 6, 4};
+    
+    cout<<maxProfit(prices)<<endl;
+    return 0;
+}
