c1d21bc64f0a0b50aee1499d90912623b17023a0
jasraj gupta <jasrajgupta1@gmail.com>
2025-10-12 18:54:06 +0530

    power(x,n)

---
+++ Arrays/mediumlevel/powerxandn.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+#include<climits>
+#include<algorithm>
+
+double myPow(double x , int n ){
+    long binForm = n;
+    if(n < 0){
+        x = 1/x;
+        binForm = -binForm;
+    }
+
+    double ans = 1;
+    while(binForm > 0){
+        if(binForm % 2 == 1){
+            ans *= x;
+           
+        }
+         x *= x;
+         binForm /= 2;
+    }
+    return ans;
+}
+
+
+
+ int main() {
+   double x = 2.00000;
+   int n = 10;
+
+   cout<<myPow(x, n)<<endl;
+ }
