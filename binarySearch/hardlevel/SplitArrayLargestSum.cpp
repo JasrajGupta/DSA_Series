1aeeef745513ebe5172b7a27a2d6615130f43def
jasraj gupta <jasrajgupta1@gmail.com>
2025-10-19 19:25:29 +0530

    split array largest sum

---
+++ binarySearch/hardlevel/SplitArrayLargestSum.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<climits>
+#include <algorithm>
+using namespace std;
+
+bool isValid(vector<int>& nums, int n, int k, int maxAllowedSum){
+    int Sum = 0, subarray = 1;
+    for(int i = 0; i<n; i++){
+        if( nums[i] > maxAllowedSum){
+            return false;
+        }
+        if(Sum + nums[i] <= maxAllowedSum){
+            Sum += nums[i];
+        }
+        else{
+            subarray++;
+            Sum = nums[i];
+        }
+}
+return subarray <= k ? true : false;
+}
+int SplitArrayLargestSum(vector<int>& nums, int n, int k){
+    if(k>n){
+        return -1;
+    }
+    int sum = 0;
+    for(int i = 0; i<n; i++){
+        sum += nums[i];
+    }
+    int ans = -1;
+    int st = 1 /* *max_element(nums.begin(), nums.end())*/, end = sum , mid;
+    while(st<=end){
+        mid = st + (end - st)/2;
+        if(isValid(nums, n, k, mid)){
+            ans = mid;
+            end = mid - 1;
+        }
+        else{
+            st = mid + 1;
+        }
+
+}
+return ans;
+}
+
+int main() {
+    vector<int> nums = {1,2,3,4,5};
+    cout<<SplitArrayLargestSum(nums, 5, 2)<<endl;
+}
