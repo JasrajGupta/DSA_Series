178798d543c348be9786f38cd94acc61ad0798d7
jasraj gupta <jasrajgupta1@gmail.com>
2026-06-19 14:37:27 +0530

    new problems

--- Graphs/graphsbasic.cpp
+++ Graphs/graphsbasic.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<list>
 #include<queue>
+#include<vector>
 using namespace std;
 
 class Graph{
@@ -45,19 +46,39 @@ public:
     }
     cout<<endl;
   }
+  void dfsHelper(int u, vector<bool> &vis){
+    cout<<u<<" ";
+    vis[u] = true;
+
+    for(int v : l[u]){
+      if(!vis[v]){
+        dfsHelper(v, vis);
+      }
+    }
+  }
+  void dfs(){
+    int src = 0;
+    vector<bool> vis(v, false);
+
+    dfsHelper(src, vis);
+  }
 
 };
 int main(){
     Graph g(5);
-    
-    g.addEdge(0, 1);
-    g.addEdge(0, 2);
-    g.addEdge(0, 4);
-    g.addEdge(1,2);
-    g.addEdge(1, 3);
-    g.addEdge(2, 3);
-    g.addEdge(3, 4);
-    g.addEdge(2, 4);
 
-    g.printAdjlist();
+  g.addEdge(0, 1);
+  g.addEdge(1, 2);
+  g.addEdge(2, 4);
+  g.addEdge(2, 3);
+  g.addEdge(1, 3);
+  
+  
+
+  cout<<"dfs : ";
+  g.dfs();
+
+  cout<<"bfs : ";
+  g.bfs();
+
 }
